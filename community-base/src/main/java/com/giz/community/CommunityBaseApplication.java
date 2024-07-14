7d1c8a2f90bc21e4d8de3e9ff63a45f22a864ee3
gzq44 <1172623242qq.com>
2024-07-14 18:23:52 +0800

    creat community-base for base feature001.

---
+++ community-base/src/main/java/com/giz/community/CommunityBaseApplication.java
@@ -0,0 +1,13 @@
+package com.giz.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityBaseApplication.class, args);
+    }
+
+}
